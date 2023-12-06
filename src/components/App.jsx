ba9c65d4039216da41ca6ff13b32d22ea817c5b8
Potyomko Veronika <vnpotomko@gmail.com>
2023-12-06 16:55:42 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import { Phonebook } from "./Phonebook/Phonebook";
+import contactsInfo from '../contactsInfo'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Phonebook contactsInfo={contactsInfo}/>
+    );
+  }
+}
